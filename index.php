17c7ca9e4943ed4109304ae3e3ee78feca69e65a
Francois Vaillant <frvaillant@gmail.com>
2025-11-25 15:46:35 +0100

    Feature: add muted to allow autostart

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
 <body>
 
 <div id="video-container">
-    <video id="video" controls autoplay></video>
+    <video id="video" controls autoplay muted playsinline></video>
 
     <button id="analyze-button">🔍 Identifier un oiseau</button>
 
